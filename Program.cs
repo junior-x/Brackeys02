2a5bf743c38e2435a14a0b27a882360701f4dcd1
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-04-01 23:03:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace aulaBrakeys02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Skynet";
+            string yo1 = Console.ReadLine();
+            string yo2 = Console.ReadLine();
+
+
+            Console.WriteLine("Hello World!");
+            ///Console.ReadKey("enter");
+            
+            Console.WriteLine("Hello Again");
+
+            Console.WriteLine("Nao aguento mais escrever console write line carai.");
+            Console.ReadKey();
+
+            if( yo1 == "poise"){
+
+                Console.WriteLine("Poise?");
+                ///Console.ReadKey("Enter");
+            }else{
+                Console.WriteLine("Poise nao");
+                ///Console.ReadKey("space");
+            }
+
+
+
+        }
+    }
+}
